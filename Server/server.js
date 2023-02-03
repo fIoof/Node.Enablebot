56bed52478bd4bd7c4711da7199c5a6a43a27553
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-03 11:13:27 +0000

    2nd commit

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import {Configuration, OpenAIApi} from 'openai'
 
 dotenv.config();
+console.log(process.env.OPENAI_API_KEY)
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
--- client/script.js
+++ client/script.js
@@ -11,7 +11,7 @@ function loader(element){
 
     loadInterval = setInterval(() => {
         element.textContent += '.';
-        if (element.textContext === '....') {
+        if (element.textContent === '....') {
             element.textContent = '';
         }
     },300)
