dfd7abc3aa980ce8157e319370802a407d7eeeef
lhargreavesEnable <lhargreaves@enable.services>
2023-02-13 15:53:04 +0000

    copy-css

--- client/script.js
+++ client/script.js
@@ -47,7 +47,7 @@ function chatStripe(isAi, value, uniqueId) {
                         alt="${isAi ? 'bot' : 'user'}"   
                     />  
         </div> 
-         <div class="message" id=${uniqueId}>${value}<img class="copy" src="${copy}" alt ='copy'/></div>       
+         <div class="message" id=${uniqueId}>${value}</div>       
         </div>
         
         ` //this creates the message that is generated
