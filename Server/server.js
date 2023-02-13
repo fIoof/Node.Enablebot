65b336e7c213f5352ac6cd38a8d4714059dd76da
lhargreavesEnable <lhargreaves@enable.services>
2023-02-13 15:58:39 +0000

    error message

--- Server/server.js
+++ Server/server.js
@@ -34,7 +34,7 @@ app.post('/', async (req, res) => {
             bot: response.data.choices[0].text
         })
     } catch (error){ //catches errors if response takes longer than timer
-        res.status(500).send({error: "Opps something went wrong!"})
+        res.status({error : "something broke"})
     }
 })
 
