ec0d12dcb5c5b8797ab26296d3dac0661f27af32
lhargreavesEnable <lhargreaves@enable.services>
2023-02-14 15:40:17 +0000

    copytoclipboard

--- client/script.js
+++ client/script.js
@@ -105,7 +105,7 @@ form.addEventListener('keyup',(e) => { //listens for when we press the enter key
 })
 
 function copyToClipBoard(){
-    var copiedText = bot 
-    copiedText[0].select();
+    var copiedText = data.bot.trim() 
+    copiedText.select();
     navigator.clipboard.writeText(copiedText.value)
 }
