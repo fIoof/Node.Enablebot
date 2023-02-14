7416ff4440fe1ed5e57a39048e344eae59ddeb97
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-14 15:49:36 +0000

    Removing error and formatting

--- client/script.js
+++ client/script.js
@@ -52,7 +52,11 @@ function chatStripe(isAi, value, uniqueId) {
         
         ` //this creates the message that is generated
     )
-    
+    function copyToClipBoard(){
+        var copiedText = document.getElementById("parsedData").innerText
+        copiedText.select();
+        navigator.clipboard.writeText(copiedText.value)
+    }
 }
 
 const handleSubmit = async (e) => {
