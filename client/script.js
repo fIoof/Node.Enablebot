c6145c9cf510914b442301c7a5be73244191620c
lhargreavesEnable <lhargreaves@enable.services>
2023-02-15 09:22:24 +0000

    copytoclipboard

--- client/script.js
+++ client/script.js
@@ -47,7 +47,7 @@ function chatStripe(isAi, value, uniqueId) {
                         alt="${isAi ? 'bot' : 'user'}"   
                     />  
         </div> 
-         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" onclick="${copyToClipBoard()}" src="${copy}"/>      
+         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" src="${copy}"/>      
         </div>
         
         ` //this creates the message that is generated
@@ -56,6 +56,7 @@ function chatStripe(isAi, value, uniqueId) {
     //     var copiedText = document.getElementById().innerText
     //     copiedText.select();
     //     navigator.clipboard.writeText(copiedText.value)
+    //      onclick="${copyToClipBoard()}"
     //  }
 }
 
