a47b5c50be8d97d903b2af4f95296dfacd1153c5
lhargreavesEnable <lhargreaves@enable.services>
2023-02-16 10:34:52 +0000

    copytoclipboard

--- client/script.js
+++ client/script.js
@@ -51,7 +51,7 @@ function chatStripe(isAi, value, uniqueId) {
                         alt="${isAi ? 'bot' : 'user'}"   
                     />  
         </div> 
-         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" onclick="navigator.clipboard.writeText(${value})" src="${copy}"/>      
+         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" onclick="navigator.clipboard.writeText('${value}')" src="${copy}"/>      
         </div>
         
         ` 
