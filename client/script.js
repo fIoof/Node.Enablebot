8f6872dbd740f9b4b8f6be6b1e34c94fc0f8b339
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-16 13:37:27 +0000

    Reformatting

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ function chatStripe(isAi, value, uniqueId) {
                         alt="${isAi ? 'bot' : 'user'}"   
                     />  
         </div> 
-         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" onclick="navigator.clipboard.writeText(parsedData) src="${copy}"/>      
+         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" onclick="navigator.clipboard.writeText('parsedData') src="${copy}"/>      
         </div>
         
         ` 
