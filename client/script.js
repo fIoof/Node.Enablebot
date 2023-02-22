2530d523f91464e1da9eaae7a2ed6237ef3a02cf
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-22 15:24:32 +0000

    Copy text

--- client/script.js
+++ client/script.js
@@ -50,7 +50,9 @@ function chatStripe(isAi, value, uniqueId,) {
                         alt="${isAi ? 'bot' : 'user'}"   
                     />  
         </div> 
-         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" src="${copy}" onclick="copyToClipboard()" />      
+         <div class="message" id=${uniqueId}>${value}</div><img class="copyimg" src="${copy}" onclick={e => {
+             console.log(e)
+         } />      
         </div>
         
         `
