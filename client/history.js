24e57dd565e748bb24ee090854ce3e6b9651b39b
lhargreavesEnable <lhargreaves@enable.services>
2023-02-23 16:00:29 +0000

    historylog

---
+++ client/history.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
