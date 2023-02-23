829b5b25bba837f06d614cebea5a782c9ede3343
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-23 14:37:16 +0000

    Copy text

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
     // User's Chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'),uniqueId2); //if user passes the data from the form
     const usercopyText = () => {
-        chatHistory.set(uniqueId, {id: uniqueId2, value: data.get('prompt')});
+        chatHistory.set(uniqueId2, {id: uniqueId2, value: data.get('prompt')});
     }
     usercopyText()
     form.reset(); // resets the data in the form so a new awnswer can be asked.
@@ -124,7 +124,6 @@ const handleSubmit = async (e) => {
         const data = await response.json(); //this gives us the actual response
         const parsedData = data.bot.trim();
         const copyText = () => {
-            counter++
             chatHistory.set(uniqueId, { id: uniqueId, value: parsedData });
         };
         copyText()
