7366e1cac31176b689be6b88092313fd7703e461
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-28 16:44:09 +0000

    History log

--- client/History.js
+++ client/History.js
@@ -1,9 +1,11 @@
 
 function openNav() {
+    console.log('test')
     document.getElementById("mySidepanel").style.width = "250px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
+    console.log('close test')
     document.getElementById("mySidepanel").style.width = "0";
 }
