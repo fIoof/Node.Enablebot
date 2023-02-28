250d994c0e82d3b86e565bb6fdab576abf57ee52
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-02-28 16:16:16 +0000

    History log

--- client/History.js
+++ client/History.js
@@ -1,8 +1,10 @@
 
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
+    console.log(openNav())
 }
 
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+    console.log(closeNav())
 }
