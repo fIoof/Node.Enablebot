a4abc0d6e38a926f71f5e87f96a3976312aa99e3
lhargreavesEnable <lhargreaves@enable.services>
2023-03-07 08:54:55 +0000

    historylog

--- client/history.js
+++ client/history.js
@@ -1,7 +1,14 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+
+var btn = document.querySelector('.toggle');
+var btnst = true;
+btn.onclick = function() {
+    if(btnst == true) {
+        document.querySelector('.toggle span').classList.add('toggle');
+        document.getElementById('sidebar').classList.add('sidebarshow');
+        btnst = false;
+    }else if(btnst == false) {
+        document.querySelector('.toggle span').classList.remove('toggle');
+        document.getElementById('sidebar').classList.remove('sidebarshow');
+        btnst = true;
+    }
+}
