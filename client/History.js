6a9f8e7194f5d72c0ecb216e117d8582b213acc9
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-03-07 14:23:37 +0000

    History log

--- client/History.js
+++ client/History.js
@@ -6,7 +6,8 @@ btn.onclick = function() {
         document.querySelector('.toggle').classList.add('toggle');
         document.getElementById('sidebar').classList.add('sidebarshow');
         btnst = false;
-    }else if(btnst === false) {
+    }else {
+        console.log('Closing sidebar...');
         document.querySelector('.toggle').classList.remove('toggle');
         document.getElementById('sidebar').classList.remove('sidebarshow');
         btnst = true;
