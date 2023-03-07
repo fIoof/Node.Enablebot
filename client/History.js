2dc72d40950aeb45da71254d970ccb5aa4713b11
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-03-07 14:19:55 +0000

    History log

--- client/History.js
+++ client/History.js
@@ -1,12 +1,12 @@
 
-var btn = document.querySelector('.toggle');
-var btnst = true;
+let btn = document.querySelector('.toggle');
+let btnst = true;
 btn.onclick = function() {
-    if(btnst == true) {
+    if(btnst === true) {
         document.querySelector('.toggle').classList.add('toggle');
         document.getElementById('sidebar').classList.add('sidebarshow');
         btnst = false;
-    }else if(btnst == false) {
+    }else if(btnst === false) {
         document.querySelector('.toggle').classList.remove('toggle');
         document.getElementById('sidebar').classList.remove('sidebarshow');
         btnst = true;
