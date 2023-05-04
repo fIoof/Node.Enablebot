7918be99b89d08ae4c4d0862b30c886eb44b5f20
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-05-04 14:04:17 +0100

    History.js edit

--- client/History.js
+++ client/History.js
@@ -1,4 +1,4 @@
-import toggleSidebar from '../../../OneDrive/PC files 2016/Documents/client/script.js'
+import toggleSidebar from './script.js'
 let btn = document.querySelector('.toggle');
 let btnst = true;
 btn.onclick = function() {
