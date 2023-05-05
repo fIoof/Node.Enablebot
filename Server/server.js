a177cdd772b77141c84601b93437d86dcae03f3f
Becca Ashington <111860418+fIoof@users.noreply.github.com>
2023-05-05 15:47:00 +0100

    changes

--- Server/server.js
+++ Server/server.js
@@ -37,4 +37,4 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000/'));
+app.listen(5000, () => console.log('Server is running on port https://node-enablebot-24qam5w7z-fioof.vercel.app/'));
--- client/script.js
+++ client/script.js
@@ -108,7 +108,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId);
     loader(messageDiv);
     // Fetch data from the server (bot's response)
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://node-enablebot-24qam5w7z-fioof.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
